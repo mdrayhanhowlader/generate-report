d9ad21963d2edd70933ea982b5ca92b0c1e7f5ed
mdrayhanhowlader <rhowlader5314@gmail.com>
2024-04-20 00:59:07 +0800

    Table for Generate report has been created

---
+++ src/app/components/generated_table.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const GeneratedTable = ({onClose}) => {
+  return (
+    <div>GeneratedTable</div>
+  
+  )
+}
+
+export default GeneratedTable
--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import PaginationExample from './generated_table';
 
-export default function Modal({ children, onClose }) {
+export default function Modal({ onClose }) {
   return (
-    <div className="fixed inset-0 z-50 overflow-y-auto bg-gray-500 bg-opacity-75"> {/* Modal backdrop */}
-      <div className="fixed top-0 left-0 right-0 mx-auto w-full max-w-sm bg-white shadow-md rounded-xl p-4">
-        {children} {/* Modal content */}
-        <button onClick={onClose}>Close</button>
-      </div>
+    <div className="fixed inset-0 z-50 overflow-y-auto bg-gray-500 bg-opacity-75">
+    <div className="fixed top-0 left-0 max-h-lg mt-10 w-screen right-0 mx-auto max-w-6xl bg-white shadow-md rounded-xl p-4">
+    
+      <PaginationExample onClose={onClose} />
+      
     </div>
+  </div>
   );
 }
